2f1fbc0da63c3e71108c77ff2ff1faf0f2813427
Daniil <mizonov2000e@gmail.com>
2023-08-29 13:54:44 +0400

    Это наш первый коммит

---
+++ les2/script.js
@@ -0,0 +1,49 @@
+// var number = 5;
+// var string = "Hello!"
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// let something;
+
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp']
+
+// console.log(arr[2]);
+
+// // alert("Hello world!");
+
+// let answer = +prompt("Есть ли вам 18", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+// console.log(4 + "- object");
+
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+
+let isCheked = false,
+    isClose = false;
+console.log(isCheked || !isClose);
+
+
